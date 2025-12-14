61d16fb1c68905cecc6de566a3a171158cc18f05
Sharif Giraldo <sharifgiraldo23@gmail.com>
2025-12-14 14:05:04 -0500

    feat: material de las clases 1 y 2
    Se añaden al repositorio el material de clase de las primeras 2 sesiones

---
+++ 1-Modulo1/Clase-2/variablesJava/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a; //Declaración
+        a = 10; //Inicialización
+        int b = 5; //Declaración e inicialización
+        a = 20; //Reasignació
+
+        byte c = 20;
+        short s  = -30000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.99;
+        char ch = 'A';
+        boolean bool = true;
+
+    }
+}
